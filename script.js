f9414a0f9f0354a2ad04e75cc2dfcc2bd5a7a1b2
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-15 18:56:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+const fn = document.querySelector("#firstNameInput");
+const ln = document.querySelector("#lastNameInput");
+const pn = document.querySelector("#phoneNumberInput");
+const ei = document.querySelector("#emailInput");
+
+form.addEventListener("submit" , (e) => {
+	e.preventDefault();
+
+	alert(`First Name: ${fn.value} Last Name: ${ln.value} Phone Number: ${pn.value} Email ID: ${ei.value}`);
+	form.reset();
+});
